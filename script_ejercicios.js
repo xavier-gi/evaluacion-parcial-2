126f1941f4ab2df8cc1e5cc441a1cb6d68f28ed9
xavier <xavierpardo876@gmail.com>
2026-07-24 19:09:49 -0500

    Ejercicio 2: variables y tipos de datos

---
+++ script_ejercicios.js
@@ -0,0 +1,11 @@
+Ejercicio 2: variables y tipos de datos
+let nombreProducto1 = "Telefono";
+let precioProducto1 = 300;
+let disponibleProducto1 = true;
+
+let nombreProducto2 = "Laptop HP";
+let precioProducto2 = 650;
+let disponibleProducto2 = false;
+
+console.log("Producto 1:", nombreProducto1, precioProducto1, disponibleProducto1);
+console.log("Producto 2:", nombreProducto2, precioProducto2, disponibleProducto2);
